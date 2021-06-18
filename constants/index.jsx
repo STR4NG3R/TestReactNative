764771429a916ad7ad556b16405c17f3ac37c9a0
Don Roquiri <locochon9268@gmail.com>
2021-06-18 10:53:14 -0500

    Style config

--- constants/index.jsx
+++ constants/index.jsx
@@ -1,10 +1,33 @@
-export const SHADOW = {
-  shadowColor: "#000",
-  shadowOffset: {
-    width: 0,
-    height: 3,
+import { StyleSheet } from "react-native";
+export const STYLE = StyleSheet.create({
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 6,
   },
-  shadowOpacity: 0.25,
-  shadowRadius: 3.84,
-  elevation: 6,
-};
+  h1: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  h2: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  h3: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  h4: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  h5: {
+    fontSize: 10,
+    fontWeight: "bold",
+  },
+});
