39d43d6e9b59a4a5659f7cc01accf8ca7aa4aa59
Don Roquiri <locochon9268@gmail.com>
2021-06-18 01:13:21 -0500

    Combine reducers

--- store/index.js
+++ store/index.js
@@ -1,10 +1,14 @@
 const { combineReducers, createStore } = require("redux");
+import { useReducer } from "react";
 import { categoriesReducer } from "../reducers/categories";
 import { productReducer } from "../reducers/products";
+import { basketReducer } from "../reducers/basket";
 
 const reducer = combineReducers({
   categories: categoriesReducer,
   products: productReducer,
+  user: useReducer,
+  basket: basketReducer,
 });
 
 export const store = createStore(reducer);
